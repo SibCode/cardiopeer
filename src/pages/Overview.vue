4e8de83bc9030074411bb6d2dac6710eef16d3ff
SibCode <simon.bertschinger@icloud.com>
2022-06-16 21:51:06 +0200

    Update Overview.vue

--- src/pages/Overview.vue
+++ src/pages/Overview.vue
@@ -7,7 +7,7 @@
     <q-img
       src="../assets/cardiopeer/heart.png"
       fit="contain"
-      class="img-800"
+      class="img-h400"
     ></q-img>
     <p class="text-grey">Bildquelle: Wörterwolke aus bereinigten Transkripten der Befragungen</p>
       <div class="q-qb-sm text-h5">Einführung</div>
@@ -133,4 +133,7 @@ export default defineComponent({
 <style lang="sass" scoped>
 .img-800
   max-width: 800px
+
+.img-h400
+  max-height: 400px
 </style>
