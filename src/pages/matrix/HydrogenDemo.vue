517c37d5aee126a1909634a42f071c5d3ed3d226
SibCode <simon.bertschinger@icloud.com>
2022-06-16 17:26:28 +0200

    Update HydrogenDemo.vue

--- src/pages/matrix/HydrogenDemo.vue
+++ src/pages/matrix/HydrogenDemo.vue
@@ -1,8 +1,33 @@
 <template>
-    
+  <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
+    <div>
+      <div style="font-size: 30vh">
+        404
+      </div>
+
+      <div class="text-h2" style="opacity:.4">
+        Implementierung der Hydrogen-Demo nicht verfügbar.
+      </div>
+
+      <q-btn
+        class="q-mt-xl"
+        color="white"
+        text-color="blue"
+        unelevated
+        to="/"
+        label="Auf die Übersichtsseite zurückkehren."
+        no-caps
+      />
+    </div>
+  </div>
 </template>
 
-<script lang="text/javascript">
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HydrogenDemo',
+});
 </script>
 
 <style>
