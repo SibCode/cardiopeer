56a0a633918bcbee9535aefcf1348847c9a77b79
SibCode <simon.bertschinger@icloud.com>
2022-06-16 21:38:26 +0200

    npx update included, hope it fix build

--- src/pages/Overview.vue
+++ src/pages/Overview.vue
@@ -24,12 +24,22 @@
       </p>
       
       <div class="text-h6">Schritt 1: Registrierung</div>
+      <q-img
+        src="../../assets/cardiopeer/step1.png"
+        fit="contain"
+        class="img-800"
+      ></q-img>
       <p>
         Der Nutzer erfährt im Rahmen der Behandlung, Rehabilitierung oder über die Herzstiftung von dieser Webseite und Webapplikation und kann sich dort registrieren. 
         Dazu muss ein Registrierungs-Formular verfügbar stehen, welches allenfalls geprüft werden kann, woraufhin diesem Nutzer ein Login erstellt werden kann.
       </p>
       
       <div class="text-h6">Schritt 2: Anmeldung</div>
+      <q-img
+        src="../../assets/cardiopeer/step2.png"
+        fit="contain"
+        class="img-800"
+      ></q-img>
       <p>
         Der Nutzer kann sich mit seinen Anmeldungsdaten einloggen und ein Profil erstellen. Das Profil wird in Midata oder einem vergleichbaren Service gespeichert. 
         Für zertifizierte Ratgeber wird bereits bei der Registrierung vermerkt, dass diese in der Rolle eines Ratgebers die Seite besuchen und ihre Angaben könnten allenfalls etwas differenzieren.
@@ -37,6 +47,11 @@
       </p>
       
       <div class="text-h6">Schritt 3: Auswahl von Ratgeber</div>
+      <q-img
+        src="../../assets/cardiopeer/step3.png"
+        fit="contain"
+        class="img-800"
+      ></q-img>
       <p>
         Der Nutzer kann als Ratsuchender nun eine Auswahl treffen um sich mit Ratgeber in Kontakt zu setzen. Dabei werden ihm nur Ratgeber angezeigt, 
         welche auch von den Präferenzen und Profilangaben her seinen Wünschen entsprechen. Auch wenn hier mehrere Ratgeber ausgewählt werden können, 
@@ -44,6 +59,11 @@
       </p>
       
       <div class="text-h6">Schritt 4: Bestätigung oder Ablehnung durch Ratgeber</div>
+      <q-img
+        src="../../assets/cardiopeer/step4.png"
+        fit="contain"
+        class="img-800"
+      ></q-img>
       <p>
         Zertifizierte Nutzer in der Rolle des Ratgebers können einsehen, welche Nutzer sie als Ratgeber wünschen. 
         Dabei können sie diese Anfragen jeweils Annehmen und eine Einladung senden, oder die Anfrage ablehnen. 
@@ -53,17 +73,35 @@
       </p>
       
       <div class="text-h6">Schritt 5: Generierung eines privaten Chat-Raums</div>
+      <q-img
+        src="../../assets/cardiopeer/step5.png"
+        fit="contain"
+        class="img-800"
+      ></q-img>
       <p>
         Sobald die Einladung bestätigt wurde kann der Chat-Raum generiert werden. Dabei wird vorgeschlagen, dass ein Applikationsserver mit einem Roboter-Nutzer als Chat-Bot
         die Einladungen und Räume verwaltet. Dabei kann dieser, wie hier unter Matrix - Implementierung beschrieben, für die gematchten Nutzer einen Raum erstellen.
         Dies hat den Vorteil, das die Informationen über den Raum allenfalls auch in Midata persistiert werden können und Client-seitig doppelt geprüft werden kann, 
-        ob dieser Raum legitim ist, existiert und auch wirklich durch Hydrogen gerendert werden soll. Zusätzlich könnten die Profile auswählen, ob sie möchten, 
+        ob dieser Raum legitim ist, existiert und auch wirklich durch Hydrogen gerendert werden soll. 
+      </p>
+      <q-img
+        src="../../assets/cardiopeer/step5_0.png"
+        fit="contain"
+        class="img-800"
+      ></q-img>
+      <p>
+        Zusätzlich könnten die Profile auswählen, ob sie möchten, 
         dass ihre Daten zur Verbesserung des Services anonym gesammelt werden - dabei könnte der Bot den Raum nach Begrüssung und einleitenden Worten nicht direkt wieder verlassen, 
         sondern im Chat verbleiben um Daten zur Aktivität, Frequenz und Nutzung zu sammeln. Dabei müsste klar festgelegt werden was, warum gesammelt wird. 
         Ebenfalls wichtig wäre hierbei, dass der Ratgeber die vollen Raum-Rechte erhält und allenfalls selbst den Bot aus dem Chat entfernen kann, selbst wenn initial zugestimmt wurde.
       </p>
       
       <div class="text-h6">Schritt 6: Kommunikation über den privaten Chat-Raum</div>
+      <q-img
+        src="../../assets/cardiopeer/step6.png"
+        fit="contain"
+        class="img-800"
+      ></q-img>
       <p>
         Die Nutzer können nun in der Rolle des Ratgebers, bzw. in der Rolle des Ratsuchenden miteinander sicher über Matrix kommunizieren!
         Die Einbindung des Hydrogen-Clients wird unter Matrix - Implementierung angesprochen.
